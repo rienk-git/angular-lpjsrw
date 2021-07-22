daf776bbf37c7dc8630ae7d9dfd8c16eda5924db
rienk-git <rienkeisma@gmail.com>
2021-07-22 09:56:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lpjsrw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lpjsrw)
---
+++ src/app/sidenav-drawer-overview-example.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+/** @title Basic drawer */
+@Component({
+  selector: 'sidenav-drawer-overview-example',
+  templateUrl: 'sidenav-drawer-overview-example.html',
+  styleUrls: ['sidenav-drawer-overview-example.css'],
+})
+export class SidenavDrawerOverviewExample {
+
+
+  // openContainer()
+}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
